575190ee718be4dfe9381588b267e07c9dd79fea
Gaston Garcia <110359329+GastonGarcia97@users.noreply.github.com>
2023-03-16 03:34:52 -0300

    Update local.py

--- marketplace/settings/local.py
+++ marketplace/settings/local.py
@@ -1,13 +1,10 @@
 from .base import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
